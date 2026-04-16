3bb3f37c6376c5308db12db5b84da5958ba25f37
Michael Cooper <mcooperpm2@gmail.com>
2026-04-16 17:05:14 -0400

    After Task 11

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -44,7 +44,18 @@ public_users.get('/isbn/:isbn',function (req, res) {
   return res.send(JSON.stringify(books[isbn], null, 4));
  });
   
-// Get book details based on author
+// Get book details based on ISBN using async-await with Axios
+public_users.get('/async/isbn/:isbn', async function (req, res) {
+    try {
+      const isbn = req.params.isbn;
+      const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+      return res.status(200).json(response.data);
+    } catch (error) {
+      return res.status(500).json({ message: 'Error fetching book by ISBN' });
+    }
+  });
+
+ // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;
     let filteredBooks = {};
